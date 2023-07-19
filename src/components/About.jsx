7685297ba8630839147d75853016d695678225f9
Alaa Agag <email.coding1@gmail.com>
2023-07-19 10:06:15 -0400

    Fixed some CSS

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -60,7 +60,7 @@ const About = () => {
 
         </motion.p>
 
-        <div className='mt-20 flex flex-wrap gap-10'>
+        <div className='mt-20 flex flex-wrap gap-10 justify-center'>
           {services.map((service, index) =>(
             <ServiceCard key={service.title} index={index} {...service} />
           ))}
